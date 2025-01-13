630d1053361f7f68c4ca72b83a096e4a3fb0b35d
Subir Mansukhani <subir.m@gmail.com>
2025-01-13 10:25:41 -0800

    comment db clear

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from app.models import Team, Event, TeamMember
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
 
     app.run(debug=True, host='0.0.0.0', port=5001)
